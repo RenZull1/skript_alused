43f5d06ace7f2266e574c72d2404e5c9f2c046c8
Renzo Limbak <renzo.limbak@khk.ee>
2021-06-01 21:12:06 +0300

    eemaldatud skript, mis oli vales kohas

--- kasutajad.sh
+++
@@ -1,9 +0,0 @@
-#----------------------------------
-#- Siin failis on kasutajad       -
-#	Renzo Limbak              -
-#----------------------------------
-
-kasutaja1
-kasutaja2
-kasutaja3
-kasutaja4
