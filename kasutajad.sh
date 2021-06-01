0921ef2a67733c9b873a723d8c2c0881aedab0c8
Renzo Limbak <renzo.limbak@khk.ee>
2021-06-01 18:27:05 +0300

    failis on kasutajate nimed

---
+++ kasutajad.sh
@@ -0,0 +1,9 @@
+#----------------------------------
+#- Siin failis on kasutajad       -
+#	Renzo Limbak              -
+#----------------------------------
+
+kasutaja1
+kasutaja2
+kasutaja3
+kasutaja4
